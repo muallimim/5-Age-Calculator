dee76aed9026f481ca9f7007831190d52bc190ac
Metin <metinhircin54@gmail.com>
2022-07-27 21:02:07 +0300

    Dogum yılından yaşı bulma formu tamamlandı.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log("Hello from console");
+function birthyear2age(){
+event.preventDefault();
+
+var birth_year;
+var current_year;
+var age;
+
+var d=new Date();
+
+
+birth_year=document.getElementById("birth_year").value;
+current_year = d.getFullYear();
+
+age = current_year - birth_year;
+document.getElementById("result").innerHTML = "Yaşınız: " + age;
+}
+
+//https://easyupload.io/j5qmkp
